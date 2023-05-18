1f062b0a6f7b68b77d2b6efb7a557ac4d01b3e01
William Bundgaard <william.bundgd@gmail.com>
2023-05-18 17:33:02 +0200

    cleand up compiler.py

--- src/compiler.py
+++ src/compiler.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2019
-# Kim Skak Larsen
-# All rights reserved.
-# License: GNU GPLv3
-
-# Description: A compiler is implemented. The implementation is meant for
-# teaching purposes. The goal is to illustrate important compiler
-# techniques in a simple setting and to enable the students to make
-# minor adjustments and extensions. The source language is a simple
-# imperative languages with integers being the only type, but including
-# expressions, assignment, control structures, and function definitions
-# and calls, including recursion and static nested scope. The target
-# language is 64 bit x86 assembler using the GNU syntax.
-
 import sys
 import getopt
 
@@ -22,40 +8,17 @@ from errors import error_message
 from lexer_parser import parser
 from symbols import ASTSymbolVisitor
 from type_checking import ASTTypeCheckingVisitor
-# from pretty_printer import ASTPrettyPrinterVisitor
-# from ast_printer import ASTTreePrinterVisitor
 from code_generation import ASTCodeGenerationVisitor
 from emit import Emit
 from symbol_table_flattener import ASTTabFlattener
 from reg_distributor import ASTRegDistributor
 from function_decoupeler import ASTFunDecouple
 
-
-__version__ = "unfinished beta"
-
+__version__ = "unfinished alpha"
 
 # MAIN
 
-def compiler(showSource, showAST, macOS, input_file, output_file):
-    """This function goes through the classic phases of a modern compiler,
-    each phase organized in its own module. The phases are:
-
-    parsing:
-        handled using the ply package - the parser module includes a lexer,
-        and it builds an abstract syntax tree (AST).
-
-    symbol collection:
-        collects function, parameter, and variables names, and stores these
-        in a symbol table.
-
-    code generation:
-        from the AST and symbol table, code is produced in an intermediate
-        representation, quite close to the final assembler code.
-
-    emit:
-        the slightly abstract intermediate code is transformed to assembler
-        code, in a macOS variant if the option is used.
-    """
+def compiler(showSource, input_file):
 
     # Read and verify ASCII input:
     encodingError = False
@@ -83,7 +46,7 @@ def compiler(showSource, showAST, macOS, input_file, output_file):
     if the_program is None:
         return ""
 
-    if showSource or showAST:
+    if showSource:
             return str(the_program)
     else:
 
@@ -118,49 +81,18 @@ def compiler(showSource, showAST, macOS, input_file, output_file):
         emitter = Emit(intermediate_code, register_distributor.getExterRegisterCount())
         emitter.emit()
         code = emitter.get_code()
-        
-        # for r in register_distributor.registers:
-        #     print(f"name = {r.name} | color = {r.color} | from {r.firstUse} to {r.lastUse} | n = {[n.name for n in r.neighbours]}")
 
         return code
-"""
-    else:
-
-        # Collect names of functions, parameters, and local variables:
-        symbols_collector = ASTSymbolVisitor()
-        the_program.accept(symbols_collector)
-
-        # Type check use of functions, parameters, and local variables:
-        type_checker = ASTTypeCheckingVisitor()
-        the_program.accept(type_checker)
-
-        # Generate intermediate code:
-        intermediate_code_generator = ASTCodeGenerationVisitor()
-        the_program.accept(intermediate_code_generator)
-        intermediate_code = intermediate_code_generator.get_code()
-
-        # Emit the target code:
-        emitter = Emit(intermediate_code,
-                       intermediate_code_generator.getLabelsGenerator(),
-                       macOS)
-        emitter.emit()
-        code = emitter.get_code()
-
-        return code
-"""
 
 def main(argv):
-    usage = f"Usage: {sys.argv[0]} [-hvsm] [-i source_file] [-o target_file]"
+
+    usage = f"Usage: {sys.argv[0]} [-hvs] [-i source_file] [-o target_file]"
     help_text = """
     -h  Print this help text.
 
     -v  Print the version number.
 
     -s  Print back the parsed source code instead of target code.
-
-    -a  Print the AST in dot format instead of target code.
-
-    -m  Generate assembly code for macOS.
 
     -i source_file  Set source file; default is stdin.
 
@@ -169,10 +101,8 @@ def main(argv):
     input_file = ""
     output_file = ""
     show_source = False
-    show_ast = False
-    macOS = False
     try:
-        opts, args = getopt.getopt(argv, "hsamvi:o:")
+        opts, args = getopt.getopt(argv, "hsvi:o:")
     except getopt.GetoptError:
         print(usage)
         sys.exit(1)
@@ -180,21 +110,20 @@ def main(argv):
         if opt == "-h":
             print(usage)
             print(help_text)
-            sys.exit(0)
+            # sys.exit(0)
         elif opt == "-v":
             print(__version__)
-            sys.exit(0)
+            # sys.exit(0)
         elif opt == "-s":
             show_source = True
-        elif opt == "-a":
-            show_ast = True
-        elif opt == "-m":
-            macOS = True
         elif opt == "-i":
             input_file = arg
         elif opt == "-o":
             output_file = arg
-    result = compiler(show_source, show_ast, macOS, input_file, output_file)
+    if input_file:
+        result = compiler(show_source, input_file)
+    else:
+        result = ""
     if output_file:
         f = open(output_file, "w")
         f.write(result)
