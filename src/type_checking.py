a32b515efe28e2d2f8d5351623dbcf45b5278bdf
Andreas Hansen <andrh20@student.sdu.dk>
2023-05-23 12:55:41 +0200

    removed a comment

--- src/type_checking.py
+++ src/type_checking.py
@@ -3,8 +3,7 @@ from visitors_base import VisitorsBase
 from errors import error_message
 from symbols import NameCategory
 
-# This module performs type checking. Since there is only one primitive
-# type, integer, not much has to be done.
+# This module performs type checking.
 # It is based on the visitors_base and AST modules that together implement
 # the recursive traversal and visit functionality.
 
